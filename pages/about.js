588172c24983e5fc1497249e18c0d18a03caf47b
Jaranchai-mac <jaranchai.nt@hotmail.com>
2020-01-24 17:28:56 +0700

    first commit

---
+++ pages/about.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+
+function AboutPage() {
+  return (
+    <Head>
+      <meta property="fb:app_id" content="1552358151700561" />
+      <meta property="og:url" content="https://eze-liff.herokuapp.com/share/share" />
+      <meta property="og:type" content="article" />
+      <meta property="og:title" content="ยังนำคืน84ไปลงช่องตัวเองเหมือนไม่มีอะไรเกิดขึ้นอีก" />
+      <meta property="og:description" content="บกวนทีมรับทราบช่วยฟ้องช่องนั้นทีครับ!!!!! เขาไม่เกรงกลัวอะไรเลยมิหนำซ้ำ" />
+      <meta property="og:image"
+        content="https://images.pexels.com/photos/3326362/pexels-photo-3326362.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="" data-react-helmet="true" />
+      <meta name="twitter:creator" content="" data-react-helmet="true" />
+      <meta name="twitter:title" content="ยังนำคืน84ไปลงช่องตัวเองเหมือนไม่มีอะไรเกิดขึ้นอีก" />
+      <meta name="twitter:description" content="บกวนทีมรับทราบช่วยฟ้องช่องนั้นทีครับ!!!!! เขาไม่เกรงกลัวอะไรเลยมิหนำซ้ำ" />
+      <meta name="twitter:image"
+        content="https://images.pexels.com/photos/3326362/pexels-photo-3326362.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
+    </Head>
+    <div>About</div>
+  )
+}
+
+export default AboutPage
---
+++ pages/share.js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+
+function SharePage() {
+  return (
+    <div>
+      <Head>
+        <meta property="fb:app_id" content="1552358151700561" />
+        <meta property="og:url" content="https://eze-liff.herokuapp.com/share/share" />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="ยังนำคืน84ไปลงช่องตัวเองเหมือนไม่มีอะไรเกิดขึ้นอีก" />
+        <meta property="og:description" content="บกวนทีมรับทราบช่วยฟ้องช่องนั้นทีครับ!!!!! เขาไม่เกรงกลัวอะไรเลยมิหนำซ้ำ" />
+        <meta property="og:image"
+          content="https://images.pexels.com/photos/3527542/pexels-photo-3527542.jpeg?cs=srgb&dl=pexels-3527542.jpg&fm=jpg" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="" data-react-helmet="true" />
+        <meta name="twitter:creator" content="" data-react-helmet="true" />
+        <meta name="twitter:title" content="ยังนำคืน84ไปลงช่องตัวเองเหมือนไม่มีอะไรเกิดขึ้นอีก" />
+        <meta name="twitter:description" content="บกวนทีมรับทราบช่วยฟ้องช่องนั้นทีครับ!!!!! เขาไม่เกรงกลัวอะไรเลยมิหนำซ้ำ" />
+        <meta name="twitter:image"
+          content="https://images.pexels.com/photos/3527542/pexels-photo-3527542.jpeg?cs=srgb&dl=pexels-3527542.jpg&fm=jpg" />
+      </Head>
+      <p>Share</p>
+    </div>
+  )
+}
+
+export default SharePage
